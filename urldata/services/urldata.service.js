1e8d9e979e86e30e71490d9280cdedd6220d2142
Daniel Bayley <danielpeterbayley@gmail.com>
2023-06-02 10:26:58 +0100

    Absolute url check implemented

--- urldata/services/urldata.service.js
+++ urldata/services/urldata.service.js
@@ -23,14 +23,18 @@ module.exports = {
 			},
 			/** @param {Context} ctx  */
 			async handler(ctx) {
-					//fetch full page, store html in string
-					const response = await fetch(ctx.params.url);
-					const fullhtml = await response.text();
-					//Remove script tags from html string, and parse html with JSDOM
-					const doc = new JSDOM(fullhtml.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')).window.document;
-					//Remove all symbols excluding numbers, letters, whitespace and newlines. Remove excess whitespace before count
-					const text = doc.body.textContent.replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s+/g, ` `).trim();
-					return {message:`SUCCESS`, fullHtmlDocWordCount: strWordCount(fullhtml), htmlBodyWordCountNoScripts: strWordCount(text), htmlBodyWordsNoScripts: text};
+				//Check if url is absolute, insert http if not
+				if (ctx.params.url.toUpperCase().indexOf("HTTP") !== 0) {
+					ctx.params.url = `http://${ctx.params.url}`;
+				}
+				//fetch full page, store html in string
+				const response = await fetch(ctx.params.url);
+				const fullhtml = await response.text();
+				//Remove script tags from html string, and parse html with JSDOM
+				const doc = new JSDOM(fullhtml.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')).window.document;
+				//Remove all symbols excluding numbers, letters, whitespace and newlines. Remove excess whitespace before count
+				const text = doc.body.textContent.replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s+/g, ` `).trim();
+				return {message:`SUCCESS`, fullHtmlDocWordCount: strWordCount(fullhtml), htmlBodyWordCountNoScripts: strWordCount(text), htmlBodyWordsNoScripts: text};
 			}
 		}
 	}
